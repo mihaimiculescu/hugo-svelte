41eb62e57dfc83dae7cd3cad0924a82b1a6d5cf1
Mihai MICULESCU <mihaimiculescu@yahoo.com>
2019-10-15 04:00:37 +0300

    Update init.sh

--- init.sh
+++ init.sh
@@ -1,1 +1,13 @@
+#!/bin/sh
+#start your new hugo&svelte project witn this
 
+if [ ! -d /sveltedev/visible/svelte ]; then
+cd /sveltedev/visible
+cp -R /sveltedev/svelte . 
+cd svelte
+mkdir assets content data public static themes
+fi
+cd /sveltedev
+npm run dev
+
+#TODO	 address potential conflict for the public folder - both svelte and hugo create and use it
